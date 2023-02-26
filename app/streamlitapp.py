e66f1746eebdf60796a3edd294f826455ef1e1af
Raviteja Tanikella <ravitejavr28@gmail.com>
2023-02-26 19:39:56 +0530

    Updated

---
+++ app/streamlitapp.py
@@ -0,0 +1,76 @@
+#Importing dependencies
+import streamlit as st
+import os
+import imageio
+import tensorflow as tf
+
+from utils import load_alignments, num_to_char, load_data, load_data_gif
+from modelutil import load_model
+
+# Set the layout to the streamlit app as wide 
+st.set_page_config(layout='wide')
+
+# https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png
+
+
+# Setup the sidebar
+with st.sidebar: 
+    st.image('https://www.onepointltd.com/wp-content/uploads/2019/12/ONE-POINT-01-1.png')
+    st.title('Whisperly')
+    st.write('This application is developed using deep learning model **_LipNet_**.')
+    st.info('The feature of uploading random videos to generate text can be added. Will be added soon!..')
+
+st.title('LipReading App') 
+# Generating a list of options or videos 
+options = os.listdir(os.path.join('..', 'data', 's1'))
+selected_video = st.selectbox('Choose a video', options)
+
+# Generate two columns 
+col1, col2 = st.columns(2)
+
+if options: 
+
+    # Rendering the video 
+    with col1: 
+        st.subheader('Converted video to .mp4 format')
+        file_path = os.path.join('..','data','s1', selected_video)
+        os.system(f'ffmpeg -i {file_path} test_video.mp4 -y')
+        print('Succesful!')
+
+        # Rendering inside of the app
+        video = open('test_video.mp4', 'rb') 
+        video_bytes = video.read() 
+        st.video(video_bytes)
+
+
+    with col2: 
+        st.subheader("Generate the spoken text")
+        button = st.button("Generate",type = 'primary')
+        if button:
+            # st.info('This is all the machine learning model sees when making a prediction')
+            print(file_path)
+            video, annotations = load_data_gif(tf.convert_to_tensor(file_path))
+
+            imageio.mimsave('animation.gif', video, fps=10)
+            # st.image('animation.gif', width=350) 
+
+            # st.info('This is the output of the machine learning model as tokens')
+            model = load_model()
+            yhat = model.predict(tf.expand_dims(video, axis=0))
+            decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
+            # st.text(decoder)
+
+            # Convert prediction to text
+            # st.info('Decode the raw tokens into words')
+
+            converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
+            st.text(converted_prediction)
+            st.header('')
+            with st.expander("How the deep learning model works?"):
+                st.write("**This is what the deep learning model sees when making a prediction**")
+                st.image('animation.gif', width=350)
+                st.info('The model generates the text based on the generated gif, which will not have any audio')
+                st.write('**This is the generated output of the deep learning model as tokens**')
+                st.text(decoder)
+                st.write('**The final text we get is**')
+                st.text(converted_prediction)
---
+++ app/utils.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from typing import List
+import cv2
+import os 
+
+vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
+char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
+# Mapping integers back to original characters
+num_to_char = tf.keras.layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+)
+
+def load_video(path:str) -> List[float]: 
+
+    cap = cv2.VideoCapture(path)
+    frames = []
+    for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))): 
+        ret, frame = cap.read()
+        frame = tf.image.rgb_to_grayscale(frame) #for less pre-processing data
+        frames.append(frame[190:236,80:220,:])#static slicing function to isolate the mouth region in the videos
+    cap.release()
+    
+    mean = tf.math.reduce_mean(frames)
+    std = tf.math.reduce_std(tf.cast(frames, tf.float32))
+    return tf.cast((frames - mean), tf.float32) / std #standerdize or scale our particular image feature by using mean and std
+    
+def load_alignments(path:str) -> List[str]: 
+    with open(path, 'r') as f: 
+        lines = f.readlines() 
+    tokens = []
+    for line in lines:
+        line = line.split()
+        if line[2] != 'sil': #To ignore silence
+            tokens = [*tokens,' ',line[2]]
+    return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
+
+def load_data(path:str):
+    path = bytes.decode(path.numpy())
+    file_name = path.split('/')[-1].split('.')[0]
+    #file_name = path.split('\\')[-1].split('.')[0]
+    video_path = os.path.join('data','s1',f'{file_name}.mpg')
+    alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
+    frames = load_video(video_path)
+    alignments = load_alignments(alignment_path)
+
+def load_data_gif(path:str):   #To generate gif
+    path = bytes.decode(path.numpy())
+    file_name = path.split('/')[-1].split('.')[0]
+    #file_name = path.split('\\')[-1].split('.')[0]
+    video_path = os.path.join('..','data','s1',f'{file_name}.mpg')
+    alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
+    frames = load_video(video_path)
+    alignments = load_alignments(alignment_path)
+    
+    return frames,alignments
